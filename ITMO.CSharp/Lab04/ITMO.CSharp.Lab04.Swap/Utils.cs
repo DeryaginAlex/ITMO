7e7e13f932cd284ec1ae5984ec7771743dd83de2
DeryaginAlex <92026368+DeryaginAlex@users.noreply.github.com>
2022-05-23 14:30:39 +0300

    add lab 4

---
+++ ITMO.CSharp/Lab04/ITMO.CSharp.Lab04.Swap/Utils.cs
@@ -0,0 +1,9 @@
+﻿namespace Utils {
+    public class Utils {
+        public static void Swap(ref int a, ref int b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
