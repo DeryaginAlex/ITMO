e1c7254f7fe1ec3260cf659978b90aa0db01f6e8
DeryaginAlex <92026368+DeryaginAlex@users.noreply.github.com>
2022-05-23 22:38:12 +0300

    add 5 lab

---
+++ ITMO.CSharp/Lab05/ITMO.CSharp.Lab05.FileDetails/FileDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ITMO.CSharp.Lab05.FileDetails {
+    internal class FileDetails {
+        static void Main(string[] args) {
+            Console.Write("Length: " + args.Length + "\n");
+            Console.Write("args: ");
+            foreach(string arg in args) {
+                Console.Write(arg + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ITMO.CSharp/Lab05/ITMO.CSharp.Lab05.ReadingFromFile/Summarize.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharp.Lab05.ReadingFromFile {
+    internal class Summarize {
+    }
+}
