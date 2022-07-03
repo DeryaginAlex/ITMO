95ff1e2f54e6a6427c7b0fc8ac1efab74580d6bf
DeryaginAlex <92026368+DeryaginAlex@users.noreply.github.com>
2022-07-03 21:22:10 +0300

    add WinForms

---
+++ ITMO.CSharp.WinForm/ITMO.CSharp.Forms.Lab01/ITMO.CSharp.Forms.Lab01.Exercise03. Inheritance/ITMO.CSharp.Forms.Lab01.Exercise02/nForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITMO.CSharp.Forms.Lab01.Exercise02 {
+    public partial class nForm : ITMO.CSharp.Forms.Lab01.Exercise02.Form1 {
+        public nForm() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ITMO.CSharp.WinForm/ITMO.CSharp.Forms.Lab02/ITMO.CSharp.Forms.Lab02.Exercise02.Lists/ITMO.CSharp.Forms.Lab02.Exercise02/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ITMO.CSharp.Forms.Lab02.Exercise02 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e) {
+            if(peopleList.Text.Length != 0) {
+                memberList.Items.Add(peopleList.Text);
+            } else MessageBox.Show("Выберите элемент из списка или введите новый");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e) {
+            while(memberList.CheckedIndices.Count > 0)
+                memberList.Items.RemoveAt(memberList.CheckedIndices[0]);
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e) {
+            memberList.Sorted = true;
+        }
+    }
+}
